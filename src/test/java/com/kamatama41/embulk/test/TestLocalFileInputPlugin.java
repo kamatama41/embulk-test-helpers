542b6da8c9e59b9ae7ccfb629f92dbfb851dfb4b
Shinichi Ishimura <shiketaudonko41@gmail.com>
2017-01-31 09:25:53 +0900

    Align

--- src/test/java/com/kamatama41/embulk/test/TestLocalFileInputPlugin.java
+++ src/test/java/com/kamatama41/embulk/test/TestLocalFileInputPlugin.java
@@ -17,8 +17,8 @@ public class TestLocalFileInputPlugin extends EmbulkPluginTest {
         runInput(config);
         assertRecords(
                 record(1, 32864, timestamp(2015, 1, 27, 19, 23, 49), timestamp(2015, 1, 27), "embulk"),
-                record(2, 14824, timestamp(2015, 1, 27, 19, 1, 23), timestamp(2015, 1, 27), "embulk jruby"),
-                record(3, 27559, timestamp(2015, 1, 28, 2, 20, 2), timestamp(2015, 1, 28), "Embulk \"csv\" parser plugin"),
+                record(2, 14824, timestamp(2015, 1, 27, 19, 1, 23),  timestamp(2015, 1, 27), "embulk jruby"),
+                record(3, 27559, timestamp(2015, 1, 28, 2, 20, 2),   timestamp(2015, 1, 28), "Embulk \"csv\" parser plugin"),
                 record(4, 11270, timestamp(2015, 1, 29, 11, 54, 36), timestamp(2015, 1, 29), "NULL")
         );
     }
