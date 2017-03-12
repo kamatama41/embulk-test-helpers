5ef4e35b9b83717190e2089fd686f41c5d3d602a
Shinichi Ishimura <shiketaudonko41@gmail.com>
2017-03-13 03:08:58 +0900

    - Replace to gradle-script-kotlin - Update Kotlin to 1.1.0 - Replace groupId to com.github.kamatama41

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+import com.github.kamatama41.gradle.gitrelease.GitReleaseExtension
+
+buildscript {
+    val kotlinVersion = "1.1.0"
+    extra["kotlinVersion"] = kotlinVersion
+    repositories {
+        jcenter()
+        maven { setUrl("http://kamatama41.github.com/maven-repository/repository") }
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("com.github.kamatama41:gradle-git-release-plugin:0.1.0")
+    }
+}
+
+apply {
+    plugin("idea")
+    plugin("kotlin")
+    plugin("com.github.kamatama41.git-release")
+}
+
+repositories {
+    jcenter()
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_7
+    targetCompatibility = JavaVersion.VERSION_1_7
+}
+
+val kotlinVersion: String by extra
+dependencies {
+    compile("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
+    compile("org.embulk:embulk-standards:0.8.16")
+    compile("org.embulk:embulk-test:0.8.16")
+    testCompile("junit:junit:4.12")
+}
+
+configure<GitReleaseExtension> {
+    groupId = "com.github.kamatama41"
+    artifactId = "embulk-test-helpers"
+    repoUri = "git@github.com:kamatama41/maven-repository.git"
+    repoDir = file("${System.getProperty("user.home")}/gh-maven-repository")
+}
